94bcede9ae347bc49879f3e5a6f00eae70a678d3
www-data <www-data@kokarn.com>
2013-02-23 00:38:52 +0000

    updated buildhook

--- buildhook.php
+++ buildhook.php
@@ -1,7 +1,13 @@
 <?php
-if ( isset( $_POST[ 'payload' ] ) ) :
-    $data = json_decode( $_POST[ 'payload' ] );
-    $repository = $data->repository;
-    system( 'git --git-dir=../' . $repository->name .'/.git --work-tree=../' . $repository->name . ' pull');
-endif;
+    include( 'include/default.php' );
+
+    if ( isset( $_POST[ 'payload' ] ) ) :
+        $data = json_decode( $_POST[ 'payload' ] );
+        $repository = $data->repository;
+        if ( !isset( $_GET[ 'pull' ] ) || $_GET[ 'pull' ] == 'yes' ) :
+            $systemCall = 'git --git-dir=../' . $repository->name . '/.git --work-tree=../' . $repository->name . ' pull';
+            exec( $systemCall, $output );
+        endif;
+        Log::text( 'Repository updated by "' . $data->pusher->name . '"', $repository->name );
+    endif;
 ?>
