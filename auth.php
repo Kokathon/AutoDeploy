c65ff9cc8b29b1f0cbd25f6562d1053c1e788bd6
Kokarn Server <kokar@gmail.com>
2013-02-24 03:14:27 +0000

    fixed wrong )

--- auth.php
+++ auth.php
@@ -1,13 +1,13 @@
 <?php
 
-$name = $_POST['name'];
-$desc = $_POST['descr'];
-$redirect = urlencode('http://kokarn.com/kokathon/AutoDeploy/authed.php?name=' . $name . '&desc='. $desc);
-$get = "client_id=115d413f5ad3eb4c0a01&scope=repo&redirect_uri=" . $redirect;
-$url = 'https://github.com/login/oauth/authorize?' . $get;
+if (isset($_POST['name']) && isset($_POST['descr'])) {
+	$name = $_POST['name'];
+	$desc = $_POST['descr'];
+	$redirect = urlencode('http://kokarn.com/kokathon/AutoDeploy/authed.php?name=' . $name . '&desc='. $desc);
+	$get = "client_id=115d413f5ad3eb4c0a01&scope=repo&redirect_uri=" . $redirect;
+	$url = 'https://github.com/login/oauth/authorize?' . $get;
+}
 
 header('Location: ' . $url) ;
-
-//echo '<a href="https://github.com/login/oauth/authorize?' . $get . '">auth</a>';
 
 ?>
